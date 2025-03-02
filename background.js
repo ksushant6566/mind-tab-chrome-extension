e5050660d4ee9c8c1fa5f2788d015e0174e34153
Sushant kumar <ksushant6566@gmail.com>
2025-03-02 19:21:39 +0530

    remove background script to redirect on startup

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-   chrome.runtime.onStartup.addListener(() => {
-     chrome.tabs.create({ url: "https://mindtab.in" });
-   });
-
-   chrome.runtime.onInstalled.addListener(() => {
-     chrome.tabs.create({ url: "https://mindtab.in" });
-   });
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.tabs.create({ url: "https://mindtab.in" });
+});
